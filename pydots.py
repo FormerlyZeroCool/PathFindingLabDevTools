22711e75d60a368f8ac59ab87a6151fc4627d3a9
andrewrubinstein <andrew.rubinstein93@gmail.com>
2021-08-11 18:04:43 -0400

    Bug fix rendering grid, and added feature to Field class to set square dimensions

--- pydots.py
+++ pydots.py
@@ -1,11 +1,16 @@
+
+
 import sys
-import pygame 
-from pygame.locals import *
+import pygame
+#automatically set later in program
+dim = -1
+
 #Globals for dimensions rendering uses
 #change odim to change the size of the grid
 odim = 300
-offset = 100/2
-dim = (odim - offset*2)/3
+gridDim = 4
+offset = 45
+
 #init objects for pygame
 pygame.init()
 pygame.font.init()
@@ -18,12 +23,113 @@ startTag = "from"
 endTag = "to"
 #you can play with the color settings by chaning the paramas given to each
 #pygame.Color(red, green, blue)
-gridColor = pygame.Color(255,0,0)
-dotColor = pygame.Color(5,205,175)
-laidLinesColor = pygame.Color(0,60,200)
-leaderLineColor = pygame.Color(195,0,190)
-backgroundColor = pygame.Color(255,255,255)
-buttonBackgroundColor = pygame.Color(125, 125, 125)
+#settings class for all objects in runtime
+class Settings:
+ def __init__(self, pg, surf):
+  self.menuText = "Settings"
+  self.dataFile = "data.txt"
+  self.active = False
+  self.pg = pg
+  self.surf = surf
+  self.leaderOn = True
+  self.showCoordinates = True
+  self.gridColor = pygame.Color(255,0,0)
+  self.dotColor = pygame.Color(5,205,175)
+  self.laidLinesColor = pygame.Color(0,60,200)
+  self.leaderLineColor = pygame.Color(195,0,190)
+  self.backgroundColor = pygame.Color(255,255,255)
+  self.buttonBackgroundColor = pygame.Color(55, 55, 55)
+  self.colors = [("Grid Color", self.gridColor),
+  ("Grid Dot Color", self.dotColor),
+  ("Laid Line Color", self.laidLinesColor),
+  ("Leader Color", self.leaderLineColor),
+  ("Background Color", self.backgroundColor),
+  ("Button Background Color", self.buttonBackgroundColor) ]
+  self.colors.clear()
+  self.buttons = []
+  i = 0
+  deltaY = 20
+  fontSize = 15
+  for color in self.colors:
+    self.buttons.append(Button(self, self , pg, surf, color[0], fontSize, odim/10, (deltaY+2)*i+dim/3, int(odim/2)+dim, deltaY))
+    i += 1
+  self.leaderButton = Button(self, self , pg, surf, "Show Leader Line", fontSize, odim/10, (deltaY+2)*i+dim/3, int(odim/2)+dim, deltaY)
+  i += 1
+  self.coordinatesButton = Button(self, self , pg, surf, "Show Current Coordinates", fontSize, odim/10, (deltaY+2)*i+dim/3, int(odim/2)+dim, deltaY)
+  i += 1
+  self.menuButton = Button(self, self , pg, surf, "Back to Menu", fontSize, odim/10, odim - (deltaY+5), int(odim/2)+dim, deltaY)
+  i += 1
+
+ def eventHandler(self, event):
+  if event.type == self.pg.MOUSEBUTTONUP:
+   self.mouseClicked(event)
+ def mouseClicked(self, event):
+  if self.menuButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
+    self.active = False
+  elif self.leaderButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
+    self.leaderOn = not self.leaderOn
+  elif self.coordinatesButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
+    self.showCoordinates = not self.showCoordinates
+ def draw(self):
+   color = self.buttonBackgroundColor
+   if self.leaderOn:
+     self.buttonBackgroundColor = pygame.Color(55, 55, 155)
+   self.leaderButton.draw()
+   self.buttonBackgroundColor = color
+   if self.showCoordinates:
+     self.buttonBackgroundColor = pygame.Color(55, 55, 155)
+   self.coordinatesButton.draw()
+   self.buttonBackgroundColor = color
+   self.buttonBackgroundColor = pygame.Color(55, 125, 55)
+   self.menuButton.draw()
+   self.buttonBackgroundColor = color
+   for button in self.buttons:
+     button.draw()
+  
+def absPoint(p):
+  #removes incoherent +- values from student work
+  p.sx = abs(p.sx)
+  p.sy = abs(p.sy)
+  p.ex = abs(p.ex)
+  p.ey = abs(p.ey)
+def loadDataFile(filepath):
+ file = open(filepath,'r')
+ data = file.read()
+ file.close()
+ x = 0
+ arr = []
+ maxWidth = 0
+ maxHeight = 0
+ arr.append(Line(0,0,0,0))
+ while x < len(data):
+     point = Line(-1,-1,-1,-1)
+     start = data.find("(",x)
+     if start == -1:
+         break
+     end = data.find(",", start+1)
+     start2 = end+1
+     end2 = data.find(")", end)
+     point.sx = int(data[start+1 :  end : 1])
+     point.sy = int(data[start2 :  end2 : 1])
+     #print("start: (", point.sx, ",", point.sy,") ", sep = "", end = " ")
+     if end2 >= 0:
+         x = end2
+     else:
+         continue
+     start = data.find("(",x+1)
+     end = data.find(",", start+1)
+     point.ex = int(data[start+1 :  end : 1])
+     start = end+1
+     end = data.find(")", start)
+     point.ey = int(data[start :  end : 1])
+     #print("end: (", point.ex, ",", point.ey,")", sep = "")
+     absPoint(point)
+     arr.append(point)
+     maxWidth = max(point.sx, point.ex, maxWidth)
+     maxHeight = max(point.sy, point.ey, maxHeight)
+     x = end+1
+ return arr
+
 #record for a line
 class Line:
  def __init__(self,sx,sy,ex,ey):
@@ -32,8 +138,9 @@ class Line:
   self.ex = ex
   self.ey = ey
 class Button:
-  def __init__(self, screenObj, pg, surf, text, fontSize, x, y, width, height):
+  def __init__(self, settings, screenObj, pg, surf, text, fontSize, x, y, width, height):
     self.screenObj = screenObj
+    self.settings = settings
     self.pg = pg
     self.surf = surf
     self.text = text
@@ -44,7 +151,7 @@ class Button:
     self.offset = height/10
     self.font = pg.font.SysFont('Comic Sans MS', fontSize) 
   def draw(self):
-    self.pg.draw.rect(self.surf, buttonBackgroundColor, (self.x, self.y, self.width, self.height))
+    self.pg.draw.rect(self.surf, self.settings.buttonBackgroundColor, (self.x, self.y, self.width, self.height))
     x = self.x+self.offset
     y = self.y+self.offset
     self.surf.blit(self.font.render(self.text, False, (255, 255, 255)), (x, y))
@@ -54,17 +161,27 @@ class Button:
 
 
 class Menu:
-  def __init__(self, pg, surf, screens):
-    self.active = True
+  def __init__(self, pg, surf, settings, screens):
+    self.active = False
     self.pg = pg
     self.surf = surf
+    self.settings = settings
     self.screens = screens
     self.buttons = []
-    i = 0
+    self.title = (Button(settings, self , pg, surf, "Menu:", 25, dim, (35+2), int(odim/2)+dim, 35))
+    i = 1
     for screen in self.screens:
-      self.buttons.append(Button(screen , pg, surf, screen.menuText, 25, dim, dim*i+dim, int(odim/2)+dim, int(dim)))
+      self.buttons.append(Button(settings, screen , pg, surf, screen.menuText, 25, dim, (35+2)*i+dim, int(odim/2)+dim, 35))
       i += 1
-  def mouseClicked(self):
+  def eventHandler(self, event):
+    if self.active:
+      if event.type == self.pg.MOUSEBUTTONUP:
+        self.mouseClicked(event)
+    else:
+      for screen in self.screens:
+       if screen.active:
+         screen.eventHandler(event)
+  def mouseClicked(self, event):
     if self.active:
       for button in self.buttons:
         if button.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
@@ -72,35 +189,47 @@ class Menu:
             button.screenObj.active = True
     else:
       for button in self.buttons:
-        button.screenObj.mouseClicked(self)
+       if button.screenObj.active:
+        button.screenObj.mouseClicked(event)
   def draw(self):
     if not(self.active):
+      found = False
       for screen in self.screens:
         if screen.active:
+          found = screen
           screen.draw()
+      self.active = not(found)
     if self.active:
+      color = self.settings.buttonBackgroundColor
+      self.settings.buttonBackgroundColor = self.pg.Color(105, 55, 55)
+      self.title.draw()
+      self.settings.buttonBackgroundColor = color
       for button in self.buttons:
         button.draw()
 class Instructions:
 #setup memory for object
- def __init__(self, pg, surf):
+ def __init__(self, pg, surf, settings):
+  self.settings = settings
   self.menuText = "Instructions"
   self.pg = pg
   self.surf = surf
   self.active = False
-  self.menuButton = Button(self, pg, surf, "Menu", 15, odim-90, odim - 45, 45, 20)
+  self.menuButton = Button(settings, self, pg, surf, "Menu", 18, odim-90, odim - 45, 45, 22)
   self.font = pg.font.SysFont('Comic Sans MS', 25)  
-  self.text = ['1.) Press enter to save',' to file algorithm.txt','2.) Press 0 to delete','algorithm.','3.) Press 9 to delete','last line.','4.) Click on a point','To place a line.']
-
-
-
+  self.text = ['1.) Press 0 to delete','all laid lines.','2.) Press 9 to delete','or undo last laid line.','3.) Click on a point','To place a line.','4.) Save/Load buttons','Save/Load to data.txt']
+#generic event handler mapper for class
+ def eventHandler(self, event):
+  if event.type == self.pg.MOUSEBUTTONUP:
+   self.mouseClicked(event)
 #handler code for when the mouse clicked event occurs
- def mouseClicked(self, menu):
+ def mouseClicked(self, event):
   if self.menuButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
     self.active = False
-    menu.active = True
  def draw(self):
+  color = self.settings.buttonBackgroundColor
+  self.settings.buttonBackgroundColor = pygame.Color(55, 125, 55)
   self.menuButton.draw()
+  self.settings.buttonBackgroundColor = color
   i = 0
   for text in self.text:
    self.surf.blit(self.font.render(text, False, (0, 0, 0)), (15, 15+30*i))
@@ -109,14 +238,39 @@ class field:
 #List of lines laid down already
  cons  = []
 #setup memory for object
- def __init__(self, pg, surf):
+ def __init__(self, pg, surf, dim, settings):
+  self.dim = dim
+  self.settings = settings
   self.menuText = "Exit Menu"
-  self.menuButton = Button(self, pg, surf, "Menu", 15, odim-90, odim - 35, 45, 20)
-  self.font = pg.font.SysFont('Comic Sans MS', 20)  
+  width = 45
+  offset = 80
+  i = 0
+  self.menuButton = Button(settings, self, pg, surf, "Menu", 18, odim-(offset+(width+5)*i), odim - 30, width, 27)
+  i += 1
+  self.loadButton = Button(settings, self, pg, surf, "Load", 18, odim-(offset+(width+5)*i), odim - 30, width, 27)
+  i += 1
+  self.saveButton = Button(settings, self, pg, surf, "Save", 18, odim-(offset+(width+5)*i), odim - 30, width, 27)
+  i += 1
+  self.undoButton = Button(settings, self, pg, surf, "Undo", 18, odim-(offset+(width+5)*i), odim - 30, width, 27)
+  self.font = pg.font.SysFont('Comic Sans MS', 30)  
   self.cons.append(Line(0,0,0,0))
   self.pg = pg#pygame object
   self.surf = surf#pygame surface object
-  self.active = False
+  self.active = True
+
+ def eventHandler(self, event):
+  if event.type == self.pg.MOUSEBUTTONUP:
+   self.mouseClicked(event)
+  elif event.type == pygame.KEYDOWN:
+   if event.key == pygame.K_0:
+    self.clearMemory()
+   elif event.key == pygame.K_9:
+    self.deleteLast()
+ def loadFile(self):
+   try:
+    self.cons = loadDataFile(self.settings.dataFile)
+   except:
+    print("Error loading file data.txt does not exist")
  def deleteLast(self):
    if(len(self.cons) > 1):
     self.cons.pop()
@@ -132,58 +286,79 @@ class field:
     contents += (startTag+': ('+str(line.sx)+', '+str(line.sy)+') '+endTag+': ('+str(line.ex)+', '+str(line.ey)+')\n')
    file.write(contents)
    file.close()
+ def calcPointCollision(self, limit):
+  for y in range(self.dim):
+   for x in range(self.dim):
+    lastLine = self.cons[len(self.cons)-1]
+    if self.pg.mouse.get_pos()[0] > (x*dim+offset-limit) and self.pg.mouse.get_pos()[0] < (x*dim+offset+limit) and self.pg.mouse.get_pos()[1] > y*dim+offset-limit and self.pg.mouse.get_pos()[1] < y*dim+offset+limit:
+     if (lastLine.ex !=x or lastLine.ey != y):
+      return Line(lastLine.ex, lastLine.ey, x, y)
+  return False
 #handler code for when the mouse clicked event occurs
- def mouseClicked(self, menu):
+ def mouseClicked(self, event):
   if self.menuButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
     self.active = False
-    menu.active = True
+  elif self.loadButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
+    self.loadFile()
+  elif self.saveButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
+    self.saveToFile(self.settings.dataFile)
+  elif self.undoButton.collision(self.pg.mouse.get_pos()[0], self.pg.mouse.get_pos()[1]):
+    self.deleteLast()
   else:
-   limit = 15
-   for y in range(4):
-    for x in range(4):
-     lastLine = self.cons[len(self.cons)-1]
-     if self.pg.mouse.get_pos()[0] > (x*dim+offset-limit) and self.pg.mouse.get_pos()[0] < (x*dim+offset+limit) and self.pg.mouse.get_pos()[1] > y*dim+offset-limit and self.pg.mouse.get_pos()[1] < y*dim+offset+limit:
-      if (lastLine.ex !=x or lastLine.ey != y):
-       self.cons.append(Line(lastLine.ex, lastLine.ey, x, y))
-   for x in range(15):
-    self.pg.draw.circle(self.surf, self.pg.Color(0,0,170), self.pg.mouse.get_pos(), int(x/2+2))
+   line = self.calcPointCollision(15)
+   if line:
+    self.cons.append(line)
+  
+  for x in range(15):
+   self.pg.draw.circle(self.surf, self.pg.Color(0,0,170), self.pg.mouse.get_pos(), int(x/2+2))
    pygame.display.update()
  def draw(self):
   if(self.active):
     self.__draw()
  def __draw(self):
+  color = self.settings.buttonBackgroundColor
+  self.settings.buttonBackgroundColor = pygame.Color(55, 125, 55)
   self.menuButton.draw()
+  self.saveButton.draw()
+  self.loadButton.draw()
+  self.undoButton.draw()
+  self.settings.buttonBackgroundColor = color
   #Draw grid, and labels
-  for x in range(4):
+  for x in range(self.dim):
    self.surf.blit(self.font.render(str(x), False, (0, 0, 0)), (x*dim+offset-10, 0))
    self.surf.blit(self.font.render(str(x), False, (0, 0, 0)), (10, x*dim+offset-10))
-   self.pg.draw.line(self.surf, gridColor,(offset+dim*x,offset), (offset+dim*x, odim-offset))
-   self.pg.draw.line(self.surf, gridColor,(offset,dim*x+offset),(odim-offset,offset+dim*x)) 
+   self.pg.draw.line(self.surf, self.settings.gridColor,(offset+dim*x,offset), (offset+dim*x, (self.dim-1)*dim+offset))
+   self.pg.draw.line(self.surf, self.settings.gridColor,(offset,dim*x+offset),((self.dim-1)*dim+offset,offset+dim*x)) 
   #Draw dots
-  for y in range(4):
-   for x in range(4):
-    self.pg.draw.circle(self.surf, dotColor, (int(dim*x+offset), int(dim*y+offset)), 10)
+  for y in range(self.dim):
+   for x in range(self.dim):
+    self.pg.draw.circle(self.surf, self.settings.dotColor, (int(dim*x+offset), int(dim*y+offset)), 10)
   #Draw lines placed
   for line in self.cons:
-   self.pg.draw.line(self.surf, laidLinesColor, (line.sx*dim+offset, line.sy*dim+offset), (line.ex*dim+offset,line.ey*dim+offset), 5)
+   self.pg.draw.line(self.surf, self.settings.laidLinesColor, (line.sx*dim+offset, line.sy*dim+offset), (line.ex*dim+offset,line.ey*dim+offset), 5)
   line = self.cons[len(self.cons)-1]
   #Draw line to cursor
-  self.pg.draw.line(self.surf, leaderLineColor, (line.ex*dim+offset, line.ey*dim+offset), self.pg.mouse.get_pos(), 4)
+  if self.settings.leaderOn:
+   self.pg.draw.line(self.surf, self.settings.leaderLineColor, (line.ex*dim+offset, line.ey*dim+offset), self.pg.mouse.get_pos(), 4)
   #calulate string for cursor pos
   pos_str = 'x: '+str(int((self.pg.mouse.get_pos()[0]-offset+dim/2)//dim))+' y: '+str(int((self.pg.mouse.get_pos()[1]-offset+dim/2)//dim))
   #generate, text from string, then show with blit
   textsurface = self.font.render(pos_str, False, (0, 0, 0))
-  if self.pg.mouse.get_pos()[0] < odim/2+dim/2:
-   self.surf.blit(textsurface, (self.pg.mouse.get_pos()[0],self.pg.mouse.get_pos()[1]-35))
-  else:
-   self.surf.blit(textsurface, (self.pg.mouse.get_pos()[0]-dim, self.pg.mouse.get_pos()[1]-35))
-  
-f = field(pygame, DisplaySurf)
-insPage = Instructions(pygame, DisplaySurf)
-menu = Menu(pygame, DisplaySurf, [f, insPage])
+  if self.settings.showCoordinates:
+   if self.pg.mouse.get_pos()[0] < odim/2+dim/2:
+    self.surf.blit(textsurface, (self.pg.mouse.get_pos()[0],self.pg.mouse.get_pos()[1]-35))
+   else:
+    self.surf.blit(textsurface, (self.pg.mouse.get_pos()[0]-dim, self.pg.mouse.get_pos()[1]-35))
+
+#Don't change instatiation of screens for app
+settings = Settings(pygame, DisplaySurf)
+f = field(pygame, DisplaySurf,gridDim, settings)
+dim = round((odim-offset)/f.dim)
+insPage = Instructions(pygame, DisplaySurf, settings)
+menu = Menu(pygame, DisplaySurf, settings, [f, insPage, settings])
 while True:
 #Fill background with white each frame
- pygame.draw.rect(DisplaySurf, backgroundColor, (0,0,odim,odim))
+ pygame.draw.rect(DisplaySurf, settings.backgroundColor, (0,0,odim,odim))
 #Draw field
  menu.draw()
 #Push updates to screen
@@ -193,12 +368,5 @@ while True:
   if event.type == pygame.QUIT:
    pygame.quit()
    sys.exit()
-  elif event.type == pygame.MOUSEBUTTONUP:
-   menu.mouseClicked()
-  elif event.type == pygame.KEYDOWN:
-   if event.key == pygame.K_RETURN:
-    f.saveToFile('algorithm.txt')
-   elif event.key == pygame.K_0:
-    f.clearMemory()
-   elif event.key == pygame.K_9:
-    f.deleteLast()
+  else:
+   menu.eventHandler(event)
